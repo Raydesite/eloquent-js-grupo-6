f9ccf1b51be4ce273056279b9885ab8838886380
Willy <wflorido@gmail.com>
2021-04-05 14:28:34 -0400

    Una lista - ejercicio cap 4

--- arraylista.js
+++ arraylista.js
@@ -1,9 +1,28 @@
 function arrayALista(a){
     let list= null;
-    for (let x = a.length; x>=0; x--){
+    for (let x = a.length-1; x>=0; x--){
         list={Valor:a[x],Resto:list};
     }
 return list;
 }
 
-console.log(arrayALista([1,2,3,4]));
+
+function listaAArray (a){
+    let arr=[];
+    let b=0;
+    function cc (a) {   
+        for (let x in a){
+            if (x=="Valor") {
+                arr[b]=a.Valor;
+                b++;
+            }
+            if (x=="Resto" && a.Resto!=null) cc(a.Resto);             
+        }
+        return arr;     
+    }
+    return cc(a);
+    
+}
+
+//console.log(arrayALista([1,2,3,4]));
+console.log(listaAArray(arrayALista([1,2,3,4])));
